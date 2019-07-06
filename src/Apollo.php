824edaa5430b07090693d9a8085ab90b7c09011c
yf <291323003@qq.com>
2019-07-06 10:38:20 +0800

    up

--- src/Apollo.php
+++ src/Apollo.php
@@ -4,7 +4,27 @@
 namespace EasySwoole\ApolloConfig;
 
 
-class Apollo
-{
+use EasySwoole\Config\TableConfig;
 
+class Apollo extends TableConfig
+{
+    protected $namespace = [];
+    /*
+     * 同步配置
+     */
+    function sync()
+    {
+
+    }
+
+    function setNameSpace(array $data):Apollo
+    {
+        $this->namespace = $data;
+        return $this;
+    }
+
+    function getNameSpace()
+    {
+        return $this->namespace;
+    }
 }
